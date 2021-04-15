5875da607119ad202bdd783b25d4f445112c69b5
cchonhee <cchonheeu@gmail.comm>
2021-04-15 12:07:39 +0900

    site

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+    return (
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam assumenda ea nam omnis. Magni fugit ab quibusdam earum temporibus nisi alias suscipit vero ea esse, hic labore neque consectetur error?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam assumenda ea nam omnis. Magni fugit ab quibusdam earum temporibus nisi alias suscipit vero ea esse, hic labore neque consectetur error?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam assumenda ea nam omnis. Magni fugit ab quibusdam earum temporibus nisi alias suscipit vero ea esse, hic labore neque consectetur error?</p>
+            </div>
+        </Layout>
+    )
+}
